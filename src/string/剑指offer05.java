aedb64feee371a043ba48204b0c770a3bbd4c35b
Evader-lgb <1412343257@qq.com>
2023-10-25 01:25:32 +0800

    string 替换空格

---
+++ src/string/剑指offer05.java
@@ -0,0 +1,24 @@
+package string;
+
+/**
+ * 题目链接：https://www.yuque.com/touguo/vfeoy3/afffr0
+ */
+public class 剑指offer05 {
+    public static void main(String[] args) {
+        String s = "1024 happy！ ";
+        System.out.println(replaceSpace(s));
+    }
+
+    public static String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
