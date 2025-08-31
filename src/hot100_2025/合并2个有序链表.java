0323efbe5fbda3ccd2e28ece96159bafa4752a78
Evader-lgb <1412343257@qq.com>
2025-08-31 16:15:34 +0800

    feat(hot100_2025): 添加合并两个有序链表的代码
    - 新增了一个名为 合并2个有序链表 的类
    - 实现了递归方法 mergeTwoLists来合并两个有序链表
    - 包含了 ListNode 类的定义，用于链表节点

---
+++ src/hot100_2025/合并2个有序链表.java
@@ -0,0 +1,35 @@
+package hot100_2025;
+
+/**
+ * 思路
+ * 递归是先进后出，不断压栈，因此if else部分是小的节点的next进入递归，这样最后的方法栈就是两个数字值最大的节点做比较
+ *
+ */
+public class 合并2个有序链表 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        if (list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else {
+            list2.next = mergeTwoLists(list1,list2.next);
+            return list2;
+        }
+    }
+
+    public class ListNode {
+       int val;
+       ListNode next;
+       ListNode() {}
+       ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   }
+
+}
