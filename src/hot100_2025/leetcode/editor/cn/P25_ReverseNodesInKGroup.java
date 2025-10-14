171e00228ff6e905772718132a67901761d3f527
Evader-lgb <1412343257@qq.com>
2025-10-14 19:49:03 +0800

    feat(leetcode): 添加K个一组翻转链表题目及解法
    - 新增P25_ReverseNodesInKGroup.java文件实现k个节点一组翻转链表逻辑
    - 添加对应的题目描述文档P25_ReverseNodesInKGroup .md
    - 在IDEA配置中注册新题目文件路径与元信息
    - 实现递归方式完成链表分组翻转核心算法
    - 支持处理不足k个节点的尾部保持原顺序逻辑

--- src/hot100_2025/leetcode/editor/cn/P25_ReverseNodesInKGroup.java
+++ src/hot100_2025/leetcode/editor/cn/P25_ReverseNodesInKGroup.java
@@ -1,57 +1,57 @@
-//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。 
+//
+// k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
 //
 // 
 //
 // 示例 1： 
 // 
 // 
-//输入：head = [1,2,3,4,5], n = 2
-//输出：[1,2,3,5]
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
 // 
 //
 // 示例 2： 
 //
 // 
-//输入：head = [1], n = 1
-//输出：[]
-// 
-//
-// 示例 3： 
 //
 // 
-//输入：head = [1,2], n = 1
-//输出：[1]
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
 // 
 //
 // 
-//
-// 提示： 
+//提示：
 //
 // 
-// 链表中结点的数目为 sz 
-// 1 <= sz <= 30 
-// 0 <= Node.val <= 100 
-// 1 <= n <= sz 
+// 链表中的节点数目为 n 
+// 1 <= k <= n <= 5000 
+// 0 <= Node.val <= 1000 
 // 
 //
 // 
 //
-// 进阶：你能尝试使用一趟扫描实现吗？ 
+// 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？ 
 //
-// Related Topics 链表 双指针 👍 3193 👎 0
+// 
+// 
+//
+// Related Topics 递归 链表 👍 2665 👎 0
 
 
 package hot100_2025.leetcode.editor.cn;
 
 /**
- * 删除链表的倒数第 N 个结点
+ * K 个一组翻转链表
  * @author Jayden
- * @date 2025-10-14 19:19:57
+ * @date 2025-10-14 19:46:31
  */
-public class P19_RemoveNthNodeFromEndOfList{
+public class P25_ReverseNodesInKGroup{
 	 public static void main(String[] args) {
 	 	 //测试代码
-	 	 Solution solution = new P19_RemoveNthNodeFromEndOfList().new Solution();
+	 	 Solution solution = new P25_ReverseNodesInKGroup().new Solution();
 	 }
 	 
 //力扣代码
@@ -118,18 +118,9 @@ class Solution {
     class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
