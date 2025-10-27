be213689278927b4095ef71fef450c69fd08cba9
Evader-lgb <1412343257@qq.com>
2025-10-27 08:07:08 +0800

    feat(leetcode): 添加反转链表 II问题的解决方案- 创建了 P92_ReverseLinkedListIi.java 文件，实现了链表区间反转功能 - 编写了详细的解题思路和算法归类文档 note/P92_ReverseLinkedListIi.md - 在 editor.xml 中注册了新的 LeetCode 编辑器配置项 - 提供了基础解法和一次遍历优化解法两种实现方式 - 包含完整的测试用例和边界情况处理说明- 添加了算法学习建议和常见错误避免指南

---
+++ src/hot100_2025/leetcode/editor/cn/P92_ReverseLinkedListIi.java
@@ -0,0 +1,119 @@
+//给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链
+//表节点，返回 反转后的链表 。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], left = 2, right = 4
+//输出：[1,4,3,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [5], left = 1, right = 1
+//输出：[5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目为 n 
+// 1 <= n <= 500 
+// -500 <= Node.val <= 500 
+// 1 <= left <= right <= n 
+// 
+//
+// 
+//
+// 进阶： 你可以使用一趟扫描完成反转吗？ 
+//
+// Related Topics 链表 👍 2021 👎 0
+
+
+package hot100_2025.leetcode.editor.cn;
+
+/**
+ * 反转链表 II
+ * @author Jayden
+ * @date 2025-10-27 07:18:02
+ */
+public class P92_ReverseLinkedListIi{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P92_ReverseLinkedListIi().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        for (int i = 0; i < left-1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode start = prev.next;
+
+
+        ListNode end = prev;
+        for (int j = left; j <= right; j++) {
+            end = end.next;
+        }
+
+        ListNode successor = end.next;
+
+        end.next = null;
+
+        ListNode reversedHead = reverseList(start);
+
+        prev.next = reversedHead;
+        start.next = successor;
+
+        return dummy.next;
+    }
+
+    ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
