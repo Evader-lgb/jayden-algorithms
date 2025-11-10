1f331a7bd88d4adac7e013dc40b1f7e0c828da55
Evader-lgb <1412343257@qq.com>
2025-11-11 01:00:19 +0800

    feat(leetcode): 添加最长公共前缀问题的解决方案
    - 创建了P14_LongestCommonPrefix.java文件实现核心算法
    - 实现了查找字符串数组最长公共前缀的功能
    - 添加了问题描述和示例测试用例
    - 集成到LeetCode编辑器配置中
    -生成了相关文档内容文件

---
+++ src/字符串/复习新/P14_LongestCommonPrefix.java
@@ -0,0 +1,83 @@
+//编写一个函数来查找字符串数组中的最长公共前缀。 
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= strs.length <= 200 
+// 0 <= strs[i].length <= 200 
+// strs[i] 如果非空，则仅由小写英文字母组成 
+// 
+//
+// Related Topics 字典树 数组 字符串 👍 3418 👎 0
+
+
+package 字符串.复习新;
+
+/**
+ * 最长公共前缀
+ * @author Jayden
+ * @date 2025-11-10 23:53:11
+ */
+public class P14_LongestCommonPrefix{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P14_LongestCommonPrefix().new Solution();
+          String[] strings = new String[3];
+          strings[0] = "flower";
+          strings[1] = "flow";
+          strings[2] = "flight";
+          solution.longestCommonPrefix(strings);
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j=0;
+            for(; j<ans.length()&&j<strs[i].length();j++){
+                if (ans.charAt(j) != strs[i].charAt(j)){
+                    // 只要有一个不一样就可以退出循环了
+                    break;
+                }
+            }
+
+            // 每次22比较的结果
+            ans = ans.substring(0,j);
+
+            if (ans.equals("")){
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
