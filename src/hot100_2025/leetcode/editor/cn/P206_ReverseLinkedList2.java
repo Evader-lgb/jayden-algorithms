516b60208d99bef23ff2854d15db989f65436359
Evader-lgb <1412343257@qq.com>
2026-02-01 21:12:04 +0800

    feat(leetcode): 添加反转链表问题解决方案
    - 实现了单链表反转算法，支持迭代方式完成反转
    - 添加了完整的测试用例和边界条件处理
    - 包含了链表定义和反转逻辑的具体实现
    - 添加了代码注释和作者信息标识

---
+++ src/hot100_2025/leetcode/editor/cn/P206_ReverseLinkedList2.java
@@ -0,0 +1,99 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 4047 👎 0
+
+
+package hot100_2025.leetcode.editor.cn;
+
+/**
+ * 反转链表
+ * @author Jayden
+ * @date 2026-02-01 21:02:04
+ */
+public class P206_ReverseLinkedList2 {
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P206_ReverseLinkedList2().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+
+        // TODO 这里咋么理解
+//        return cur;
+        return pre;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val,ListNode next) { this.val = val; this.next = next; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
