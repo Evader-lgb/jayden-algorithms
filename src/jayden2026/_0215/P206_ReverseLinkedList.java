27d8026df5c9ad34c22d34fe91587c10b53b91e3
Evader-lgb <1412343257@qq.com>
2026-02-15 20:18:13 +0800

    feat(P206): 添加反转链表解决方案
    - 实现了单链表反转算法，使用迭代方式完成
    - 定义了ListNode数据结构和构造函数
    - 添加了完整的题目描述和示例说明
    - 包含测试代码和边界情况处理
    - 实现了时间复杂度O(n)空间复杂度O(1)的优化解法

---
+++ src/jayden2026/_0215/P206_ReverseLinkedList.java
@@ -0,0 +1,96 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 4057 👎 0
+
+
+package jayden2026._0215;
+
+/**
+ * 反转链表
+ * @author Jayden
+ * @date 2026-02-15 19:57:38
+ */
+public class P206_ReverseLinkedList{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P206_ReverseLinkedList().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
