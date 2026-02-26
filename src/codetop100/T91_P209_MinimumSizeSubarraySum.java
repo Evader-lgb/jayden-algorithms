14617817c96f238037b3f8ffd26a64018f0b856c
Evader-lgb <1412343257@qq.com>
2026-02-26 20:10:28 +0800

    feat(leetcode): 添加多道算法题的解决方案
    - 新增codetop100系列题目包括T91-T100共10道算法题
    - 添加hot100_2025系列题目P83、P139、P169等算法题
    - 实现滑动窗口算法解决最小长度子数组问题
    - 完成链表节点交换、二叉树翻转等经典算法
    - 更新IDEA编辑器配置文件支持新题目索引

---
+++ src/codetop100/T91_P209_MinimumSizeSubarraySum.java
@@ -0,0 +1,89 @@
+//给定一个含有 n 个正整数的数组和一个正整数 target 。 
+//
+// 找出该数组中满足其总和大于等于 target 的长度最小的 子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其
+//长度。如果不存在符合条件的子数组，返回 0 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：target = 7, nums = [2,3,1,2,4,3]
+//输出：2
+//解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：target = 4, nums = [1,4,4]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= target <= 10⁹ 
+// 1 <= nums.length <= 10⁵ 
+// 1 <= nums[i] <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。 
+// 
+//
+// Related Topics 数组 二分查找 前缀和 滑动窗口 👍 2539 👎 0
+
+
+package codetop100;
+
+/**
+ * 长度最小的子数组
+ * @author Jayden
+ * @date 2025-10-31 15:51:07
+ */
+public class T91_P209_MinimumSizeSubarraySum {
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new T91_P209_MinimumSizeSubarraySum().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int result = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= target){
+                result = Math.min(result,right - left +1);
+                sum -= nums[left];
+                left ++;
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/hot100_2025/leetcode/editor/cn/P83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,73 @@
+//给定一个已排序的链表的头
+// head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序 排列 
+// 
+//
+// Related Topics 链表 👍 1251 👎 0
+
+
+package hot100_2025.leetcode.editor.cn;
+
+/**
+ * 删除排序链表中的重复元素
+ * @author Jayden
+ * @date 2026-02-26 20:00:08
+ */
+public class P83_RemoveDuplicatesFromSortedList{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P83_RemoveDuplicatesFromSortedList().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
